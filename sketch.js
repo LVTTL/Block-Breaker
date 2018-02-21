c8b5d86451b5b9f4cb941cc02db11b4204b818c6
Moritz <LVTTL@users.noreply.github.com>
2018-02-21 11:11:10 +0100

    moved endscreens to screen.js

--- sketch.js
+++ sketch.js
@@ -219,42 +219,7 @@ function checkCollision(a, b) {
     return 0;
 }
 
-function gameOverScreen() {
-    background(120, 120, 120);
-    textSize(75);
-    textAlign(CENTER);
-    fill(255, 0, 0);
-    text("Game Over!", width/2, height/2);
-    noLoop();
-}
 
-function winningScreen() {
-    background(120, 120, 120);
-    textSize(75);
-    textAlign(CENTER);
-    fill(0, 255, 0);
-    text("Congrats!", width/2, height/2);
-    noLoop();
-}
-
-
-function errorScreen() {
-    background(120, 120, 120);
-    textSize(75);
-    textAlign(CENTER);
-    fill(255, 0, 0);
-    text("Error", width/2, height/2);
-    noLoop();
-}
-
-function pauseScreen() {
-    push();
-    textSize(75);
-    textAlign(CENTER);
-    fill(0, 255, 0);
-    text("Paused", width/2, height/2);
-    pop();
-}
 
 function keyPressed() {
     if (keyCode === 32) {
